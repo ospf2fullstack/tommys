67eb6e1088cb729dd3566d658de066ca0c2044a9
ospf2fullstack <garyinnerarity@gmail.com>
2023-09-30 21:38:44 -0500

    revert

---
+++ js/main.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var videoElement = document.getElementById("backgroundVideo");
+    videoElement.playbackRate = 1; // 50% of the original speed
+});
