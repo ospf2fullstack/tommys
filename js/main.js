f8baca0c72963da393e5d4143316515e3fa6f089
ospf2fullstack <garyinnerarity@gmail.com>
2023-09-30 21:42:46 -0500

    font

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
     var videoElement = document.getElementById("backgroundVideo");
-    videoElement.playbackRate = 1; // 50% of the original speed
+    videoElement.playbackRate = 1;
 });
